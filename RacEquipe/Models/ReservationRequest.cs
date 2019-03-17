6fee97ee9cbde966f697eaed9daa738b90e2d37d
Pierre Bélanger <e1895654@cmaisonneuve.qc.ca>
2019-03-17 14:01:23 -0400

    Classe ReservationRequest

---
+++ RacEquipe/Models/ReservationRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RacEquipe.Models
+{
+    class ReservationRequest
+    {
+        public Equipement Equipement { get; set; }
+
+        public Utilisateur Utilisateur { get; set; }
+
+        public DateTime DateFrom { get; set; }
+
+        public DateTime DateTo { get; set; }
+    }
+}
